605ac63b181c7af0cac36dbea1272c236c5a4398
Ayseta <77755376+Ayseta@users.noreply.github.com>
2021-03-06 01:43:22 +0300

    commit

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id+" "+"Müşteri Eklendi");
+        }
+
+        public void Sil(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Id + " " + "Id Numaralı Müşteri Silindi");
+
+        }
+
+        public void Listele(Musteri[] musteriler) 
+        {
+            
+            Console.WriteLine("Listeleme Türü Girin:"+ "\n\n\t Id No için 1, \n \t KrediNotu için 2 ");
+            tekrar:
+            var tur= Convert.ToInt32(Console.ReadLine());
+            switch (tur)
+            {
+                case 1:
+                    foreach (var musteri in musteriler)
+                    {
+                        Console.WriteLine(musteri.Id +" "+ "Numaralı Müşteri: "+musteri.AdSoyad);
+                    }
+                    break;
+                case 2:
+                    foreach (var musteri in musteriler)
+                    {
+                        Console.WriteLine(musteri.AdSoyad +"   " + "Kredi Notu: " + musteri.Kredibilite);
+                    }
+                    break;
+               
+                default:
+                Console.WriteLine("Tekrar Deneyin");
+                    goto tekrar;
+           
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 10001;
+            musteri1.AdSoyad = "Mesut Bereket";
+            musteri1.Kredibilite = 1800;
+         
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 10002;
+            musteri2.AdSoyad = "Şükrü Karar  ";
+            musteri2.Kredibilite = 1100;
+            
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 10003;
+            musteri3.AdSoyad = "Barbar Tacir ";
+            musteri3.Kredibilite = 500;
+
+           
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach(var musteri in musteriler) 
+            {
+                Console.WriteLine(musteri.Id+"  "+musteri.AdSoyad+"  "+musteri.Kredibilite);
+               
+              
+                if (musteri.Kredibilite < 700)
+                {
+                    Console.WriteLine("Riskli");
+                }
+                else if (musteri.Kredibilite <= 1100)
+                {
+                    Console.WriteLine("Az Riskli");
+                }
+                else if (musteri.Kredibilite < 1900)
+                {
+                    Console.WriteLine("Çok iyi");
+                }
+            }
+            Console.WriteLine("\n");
+           
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            Console.WriteLine("\t");
+            musteriManager.Sil(musteri3);
+
+            Console.WriteLine("\n");
+            musteriManager.Listele(musteriler);
+         
+            Console.ReadLine();
+        }
+    }
+}
